9040fa8634986b4a2e8f1aa63b0b4d885bfb9d33
Claas Heuer <cheuer@tierzucht.uni-kiel.de>
2014-09-07 15:40:11 +0200

    changed "editPed" to us C-function
    "editPed" now uses the C-function "get_generation" as a replacement for "getGenAncestors".
    Syntax and behaviour are preserved

--- R/pedigree.R
+++ R/pedigree.R
@@ -279,12 +279,13 @@ editPed <- function(sire, dam, label, verbose = FALSE)
     pede <- data.frame(id= label, sire= sire, dam= dam, gene=rep(NA, times=nped))
     noParents <- (is.na(pede$sire) & is.na(pede$dam))
     pede$gene[noParents] <- 0
-    for(i in 1:nped){
-        if(verbose) print(i)
-        if(is.na(pede$gene[i])){
-            id <-pede$id[i]
-            pede <-getGenAncestors(pede, id)
-        }}
+    pede$gene<-as.integer(pede$gene)
+
+# new version that calls the C-function "get_generation"
+# Note: there is no return value, the R-objects are directly being changed
+
+    .Call("get_generation", pede$sire, pede$dam, pede$id, pede$gene, as.integer(verbose))    
+
     ord<- order(pede$gene)
     ans<-data.frame(label=labelOl, sire=sireOl, dam=damOl, gene=pede$gene,
                     stringsAsFactors =F)
