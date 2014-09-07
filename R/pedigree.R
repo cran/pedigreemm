1fde6a5af1ef387802a40c6490d0a8cdf6ba288e
Claas Heuer <cheuer@tierzucht.uni-kiel.de>
2014-09-07 23:18:47 +0200

    changed "diag()" to "Diagonal()" - Sparse!

--- R/pedigree.R
+++ R/pedigree.R
@@ -182,7 +182,7 @@ getAInv <- function(ped)
 {
     stopifnot(is(ped, "pedigree"))    
     T_Inv <- as(ped, "sparseMatrix")
-    D_Inv <- diag(1/Dmat(ped))
+    D_Inv <- Diagonal(x=1/Dmat(ped))
     aiMx<-t(T_Inv) %*% D_Inv %*% T_Inv
     dimnames(aiMx)[[1]]<-dimnames(aiMx)[[2]] <-ped@label
     aiMx
